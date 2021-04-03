f61e8e560f7fb06d9463a8ade7348c64ba0d0b11
calvin <seng-lee@live.com>
2021-04-03 06:52:21 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/app';
+	
+	
+var firebaseConfig = {
+  apiKey: "AIzaSyATTDi2UxVCcDDBXpxp1QaRqtLdYNWBX0g",
+  authDomain: "pomodoro-c108c.firebaseapp.com",
+  projectId: "pomodoro-c108c",
+  storageBucket: "pomodoro-c108c.appspot.com",
+  messagingSenderId: "348966800544",
+  appId: "1:348966800544:web:2b98905a61d9c3ebc34dc5",
+  measurementId: "G-8MLFY9JN51"
+};
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
